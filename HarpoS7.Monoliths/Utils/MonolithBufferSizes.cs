2161b5134303490878c6a535445ef55147d55249
bonk-dev <github.p0j6t@passinbox.com>
2024-07-07 19:38:28 +0200

    Test size arrays' length in MonolithBufferSizes
    SrcSizes and DstSizes must have the same length

--- HarpoS7.Monoliths/Utils/MonolithBufferSizes.cs
+++ HarpoS7.Monoliths/Utils/MonolithBufferSizes.cs
@@ -2,14 +2,14 @@ namespace HarpoS7.Monoliths.Utils;
 
 public static class MonolithBufferSizes
 {
-    private static readonly int[] SrcSizes =
+    internal static readonly int[] SrcSizes =
     [
         0x12 * sizeof(uint),
         0x12 * sizeof(uint),
         0x2A * sizeof(uint)
     ];
     
-    private static readonly int[] DstSizes =
+    internal static readonly int[] DstSizes =
     [
         0x12 * sizeof(uint),
         0x05 * sizeof(uint),
