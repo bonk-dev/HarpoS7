46944922aa0b897834454f55a74cd2cd8268fb02
bonk-dev <github.p0j6t@passinbox.com>
2024-07-18 15:24:29 +0200

    Add Monolith6's WithCopy

--- HarpoS7.Monoliths.Tests/Impl/MonolithWithCopyTests.cs
+++ HarpoS7.Monoliths.Tests/Impl/MonolithWithCopyTests.cs
@@ -80,4 +80,33 @@ public class MonolithWithCopyTests
             Assert.That(out2Array, Is.EqualTo(expectedOut2Bytes));
         });
     }
+    
+    [Test]
+    public void Monolith6WithCopyTest()
+    {
+        var in1Bytes = File.ReadAllBytes(BlobUtils.GetWithCopyBlobPath(6, false, 1));
+        var in2Bytes = File.ReadAllBytes(BlobUtils.GetWithCopyBlobPath(6, false, 2));
+        var in3Bytes = File.ReadAllBytes(BlobUtils.GetWithCopyBlobPath(6, false, 3));
+        
+        var expectedOut1Bytes = File.ReadAllBytes(BlobUtils.GetWithCopyBlobPath(6, true, 1));
+        var expectedOut2Bytes = File.ReadAllBytes(BlobUtils.GetWithCopyBlobPath(6, true, 2));
+
+        Span<byte> out1Buffer = stackalloc byte[Monolith6.WithCopyOut1Size];
+        Span<byte> out2Buffer = stackalloc byte[Monolith6.WithCopyOut2Size];
+        Monolith6.WithCopy(
+            out1Buffer,
+            out2Buffer, 
+            in1Bytes.AsSpan(), 
+            in2Bytes.AsSpan(),
+            in3Bytes.AsSpan()
+        );
+
+        var out1Array = out1Buffer.ToArray();
+        var out2Array = out2Buffer.ToArray();
+        Assert.Multiple(() =>
+        {
+            Assert.That(out1Array, Is.EqualTo(expectedOut1Bytes));
+            Assert.That(out2Array, Is.EqualTo(expectedOut2Bytes));
+        });
+    }
 }
