e974d8b17d62b2a1022e2b72994d963609c82653
bonk-dev <github.p0j6t@passinbox.com>
2024-08-14 12:41:34 +0200

    Move real PLC auth logic to RealPlcAuthenticator
    This logic will be reused in the legitimation process.

---
+++ HarpoS7.Family0/Auth/RealPlcAuthenticator.cs
@@ -0,0 +1,215 @@
+using System.Buffers;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using CommunityToolkit.HighPerformance.Buffers;
+using HarpoS7.Family0.BitOperations;
+using HarpoS7.Family0.Transforms;
+using HarpoS7.Utilities.Auth;
+using HarpoS7.Utilities.Extensions;
+
+namespace HarpoS7.Family0.Auth;
+
+public class RealPlcAuthenticator : IDisposable
+{
+    private static readonly ArrayPool<byte> AesKeyPool = ArrayPool<byte>.Create(16, 2);
+
+    private const PaddingMode Padding = PaddingMode.Zeros;
+
+    private readonly MemoryOwner<byte> _iv;
+    private readonly MemoryOwner<byte> _key1;
+    private readonly MemoryOwner<byte> _key2;
+    private readonly MemoryOwner<byte> _lookupTable;
+    private readonly MemoryOwner<byte> _checksum;
+    private readonly byte[] _challengeEncryptionKey;
+    private readonly byte[] _checksumEncryptionKey;
+    private readonly Aes _aes;
+
+    private int _encryptedBytes;
+
+    public RealPlcAuthenticator()
+    {
+        _iv = MemoryOwner<byte>.Allocate(16);
+        _key1 = MemoryOwner<byte>.Allocate(24);
+        _key2 = MemoryOwner<byte>.Allocate(24);
+        _lookupTable = MemoryOwner<byte>.Allocate(Transform3.DestinationSize);
+        _checksum = MemoryOwner<byte>.Allocate(16);
+        
+        _key1.Span.FillWithCryptoRandomBytes();
+        _key2.Span.FillWithCryptoRandomBytes();
+        _iv.Span.FillWithCryptoRandomBytes();
+
+        _aes = Aes.Create();
+        _aes.Padding = Padding;
+        _aes.KeySize = 128;
+
+        _challengeEncryptionKey = AesKeyPool.Rent(_aes.KeySize / 8);
+        _checksumEncryptionKey = AesKeyPool.Rent(_aes.KeySize / 8);
+    }
+
+    public int WriteMetadata(Span<byte> destination, ReadOnlySpan<byte> publicKey, EPublicKeyFamily keyFamily)
+    {
+        if (keyFamily != EPublicKeyFamily.S71200 && keyFamily != EPublicKeyFamily.S71500)
+        {
+            throw new ArgumentException(
+                $"{Enum.GetName(keyFamily)} is not supported by this authenticator", 
+                nameof(keyFamily)
+            );
+        }
+        
+        var offset = BlobMetadataWriter.WriteMetadata(
+            destination, 
+            publicKey,
+            _key2.Span, 
+            keyFamily
+        );
+
+        return offset;
+    }
+
+    public int WriteSeed(Span<byte> destination, ReadOnlySpan<byte> publicKey)
+    {
+        var offset = 0;
+        
+        // Seed
+        Span<byte> t1 = stackalloc byte[Transform1.DestinationSize];
+        Transform1.Execute(t1, _key1.Span);
+        Transform6.Execute(destination[offset..], publicKey, t1);
+        offset += Transform6.DestinationSize;
+        
+        // Keys derivation
+        DeriveKeysAndLookupTable(t1);
+        
+        // Checksum
+        Transform4.Execute(_checksum.Span, _iv.Span, _lookupTable.Span);
+
+        return offset;
+    }
+
+    public int EncryptPart1(Span<byte> destination, ReadOnlySpan<byte> challenge)
+    {
+        // Copy the starting IV
+        var offset = 0;
+        _iv.Span.CopyTo(destination[offset..]);
+        offset += _iv.Length;
+        
+        // Encrypt 16 bytes of challenge
+        Span<byte> ciphertextBlock = stackalloc byte[_aes.BlockSize / 8];
+        // challenge.Slice(2, ciphertextBlock.Length).CopyTo(ciphertextBlock);
+        
+        _aes.EncryptEcb(
+            _iv.Span,
+            ciphertextBlock,
+            Padding
+        );
+        ciphertextBlock.Xor(challenge.Slice(2, ciphertextBlock.Length));
+        ciphertextBlock.CopyTo(destination[offset..]);
+        offset += ciphertextBlock.Length;
+        _encryptedBytes += ciphertextBlock.Length;
+        
+        // Rotate IV
+        BigIntOperations.RotateLeft31(_iv.Span);
+        UpdateChecksum(ciphertextBlock);
+        
+        // Encrypt first 16 bytes of key2
+        _aes.EncryptEcb(
+            _iv.Span,
+            ciphertextBlock,
+            Padding
+        );
+        ciphertextBlock.Xor(_key2.Span[..ciphertextBlock.Length]);
+        ciphertextBlock.CopyTo(destination[offset..]);
+        offset += ciphertextBlock.Length;
+        _encryptedBytes += ciphertextBlock.Length;
+        
+        // Rotate IV
+        BigIntOperations.RotateLeft31(_iv.Span);
+        UpdateChecksum(ciphertextBlock);
+
+        return offset;
+    }
+
+    public int EncryptPart2(Span<byte> destination)
+    {
+        Span<byte> ciphertextBlock = stackalloc byte[_aes.BlockSize / 8];
+        
+        // Encrypt last 8 bytes of key2
+        _aes.EncryptEcb(
+            _iv.Span, 
+            ciphertextBlock, 
+            Padding
+        );
+        ciphertextBlock.Xor(_key2.Span[ciphertextBlock.Length..]);
+        
+        var key2LeftOverLength = _key2.Span.Length - ciphertextBlock.Length;
+        ciphertextBlock[..key2LeftOverLength].CopyTo(destination);
+        _encryptedBytes += key2LeftOverLength;
+        var offset = key2LeftOverLength;
+        
+        // Final checksum calculation & encryption
+        ciphertextBlock[key2LeftOverLength..].Clear();
+        
+        UpdateChecksum(ciphertextBlock);
+            
+        var checksumDwords = MemoryMarshal.Cast<byte, uint>(_checksum.Span);
+        checksumDwords[3] ^= (uint)_encryptedBytes;
+        
+        Transform4.Execute(_checksum.Span, _checksum.Span, _lookupTable.Span);
+
+        _aes.Key = _checksumEncryptionKey;
+        _aes.EncryptEcb(
+            _checksum.Span,
+            ciphertextBlock,
+            PaddingMode.Zeros
+        );
+
+        // Copy the final checksum
+        ciphertextBlock.CopyTo(destination[offset..]);
+        offset += ciphertextBlock.Length;
+
+        return offset;
+    }
+
+    /// <summary>
+    /// Writes the generated key1 to the specified buffer. Key1 is used for the derivation of the session key.
+    /// </summary>
+    /// <param name="destination">The key1 buffer</param>
+    public void ExtractKey1(Span<byte> destination) => 
+        _key1.Span.CopyTo(destination);
+
+    private void DeriveKeysAndLookupTable(ReadOnlySpan<byte> t1)
+    {
+        // Generates 3, 128-bit keys
+        Span<byte> transform2Buffer = stackalloc byte[Transform2.DestinationSize];
+        Transform2.Execute(transform2Buffer, t1);
+
+        // Challenge encryption key
+        const int challengeEncryptionKeyLength = 128 / 8;
+        transform2Buffer[..challengeEncryptionKeyLength].CopyTo(_challengeEncryptionKey.AsSpan());
+
+        _aes.Key = _challengeEncryptionKey;
+
+        // Checksum encryption key
+        const int checksumEncryptionLength = 128 / 8;
+        transform2Buffer.Slice(challengeEncryptionKeyLength, checksumEncryptionLength)
+            .CopyTo(_checksumEncryptionKey.AsSpan());
+        
+        // Create the lookup table
+        Transform3.Execute(_lookupTable.Span, transform2Buffer[32..]);
+    }
+
+    private void UpdateChecksum(ReadOnlySpan<byte> ciphertextBlock)
+    {
+        _checksum.Span.Xor(ciphertextBlock);
+        Transform4.Execute(_checksum.Span, _checksum.Span, _lookupTable.Span);
+    }
+
+    public void Dispose()
+    {
+        _iv.Dispose();
+        _key1.Dispose();
+        _key2.Dispose();
+        _lookupTable.Dispose();
+        _checksum.Dispose();
+        _aes.Dispose();
+    }
+}
--- HarpoS7/Auth/LegacyAuthenticationScheme.cs
+++ HarpoS7/Auth/LegacyAuthenticationScheme.cs
@@ -1,10 +1,5 @@
-﻿using System.Buffers;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using HarpoS7.Aes;
-using HarpoS7.Family0.BitOperations;
-using HarpoS7.Family0.Transforms;
+﻿using HarpoS7.Aes;
+using HarpoS7.Family0.Auth;
 using HarpoS7.Utilities.Extensions;
 using HarpoS7.Keys;
 using HarpoS7.Seed;
@@ -17,8 +12,6 @@ namespace HarpoS7.Auth;
 /// </summary>
 public static class LegacyAuthenticationScheme
 {
-    private static readonly ArrayPool<byte> AesKeyPool = ArrayPool<byte>.Create(16, 2);
-
     public static void Authenticate(
         Span<byte> encryptedBlobData,
         Span<byte> sessionKey,
@@ -61,173 +54,15 @@ public static class LegacyAuthenticationScheme
                 nameof(encryptedBlobData));
         }
 
-        Span<byte> challengeBuffer = stackalloc byte[16];
-        challenge.Slice(2, challengeBuffer.Length).CopyTo(challengeBuffer);
-        
+        var authenticator = new RealPlcAuthenticator();
+        var offset = authenticator.WriteMetadata(encryptedBlobData, publicKey, publicKeyFamily);
+        offset += authenticator.WriteSeed(encryptedBlobData[offset..], publicKey);
+        offset += authenticator.EncryptPart1(encryptedBlobData[offset..], challenge);
+        _ = authenticator.EncryptPart2(encryptedBlobData[offset..]);
+
         Span<byte> key1 = stackalloc byte[24];
-        key1.FillWithCryptoRandomBytes();
-        
-        Span<byte> key2 = stackalloc byte[24];
-        key2.FillWithCryptoRandomBytes();
+        authenticator.ExtractKey1(key1);
 
-        #region Metadata
-
-        var offset = BlobMetadataWriter.WriteMetadata(
-            encryptedBlobData, 
-            publicKey, 
-            key2, 
-            publicKeyFamily
-        );
-
-        #endregion
-
-        #region Seed
-
-        Span<byte> t1 = stackalloc byte[Transform1.DestinationSize];
-        Transform1.Execute(t1, key1);
-        Transform6.Execute(encryptedBlobData[offset..], publicKey, t1);
-        offset += Transform6.DestinationSize;
-        
-        #endregion
-
-        #region IV
-
-        Span<byte> aesIv = stackalloc byte[16];
-        aesIv.FillWithCryptoRandomBytes();
-        aesIv.CopyTo(encryptedBlobData[offset..]);
-        offset += aesIv.Length;
-        
-        #endregion
-
-        #region Key generation
-
-        // Generates 3, 128-bit keys
-        Span<byte> transform2Buffer = stackalloc byte[Transform2.DestinationSize];
-        Transform2.Execute(transform2Buffer, t1);
-
-        // Challenge encryption key (CEK)
-        const int challengeEncryptionKeyLength = 128 / 8;
-        var challengeEncryptionKey = AesKeyPool.Rent(challengeEncryptionKeyLength);
-        transform2Buffer[..challengeEncryptionKeyLength].CopyTo(challengeEncryptionKey.AsSpan());
-
-        const int checksumEncryptionLength = 128 / 8;
-        var checksumEncryptionKey = AesKeyPool.Rent(checksumEncryptionLength);
-        transform2Buffer.Slice(challengeEncryptionKeyLength, checksumEncryptionLength)
-            .CopyTo(checksumEncryptionKey.AsSpan());
-
-        // Checksum
-        Span<byte> lookupTable = stackalloc byte[Transform3.DestinationSize];
-        Transform3.Execute(lookupTable, transform2Buffer[32..]);
-        Span<byte> checksum = stackalloc byte[Transform4.DestinationSize];
-        Transform4.Execute(checksum, aesIv, lookupTable);
-        
-        #endregion
-        
-        try
-        {
-            #region Challenge encryption
-            
-            using var aes = System.Security.Cryptography.Aes.Create();
-            aes.KeySize = 128;
-            aes.Key = challengeEncryptionKey;
-
-            const int encryptedChallengeLength = 16;
-            Span<byte> blockCiphertext = stackalloc byte[encryptedChallengeLength];
-            
-            aes.EncryptEcb(
-                aesIv,
-                blockCiphertext,
-                PaddingMode.Zeros
-            );
-            blockCiphertext.Xor(challengeBuffer);
-
-            blockCiphertext.CopyTo(encryptedBlobData[offset..]);
-            offset += blockCiphertext.Length;
-            
-            // IV gets carry-rotated after every encryption
-            BigIntOperations.RotateLeft31(aesIv);
-            
-            // XOR checksum with enc. challenge
-            checksum.Xor(blockCiphertext);
-            Transform4.Execute(checksum, checksum, lookupTable);
-
-            #endregion
-
-            #region Key2 encryption
-
-            // Encrypt key2
-            aes.EncryptEcb( 
-                aesIv, 
-                blockCiphertext,
-                PaddingMode.Zeros
-            );
-            blockCiphertext.Xor(key2[..(aes.BlockSize / 8)]);
-            
-            blockCiphertext.CopyTo(encryptedBlobData[offset..]);
-            offset += blockCiphertext.Length;
-            
-            BigIntOperations.RotateLeft31(aesIv);
-            
-            checksum.Xor(blockCiphertext);
-            Transform4.Execute(checksum, checksum, lookupTable);
-
-            #endregion
-
-            #region Key 2 encryption part 2
-            
-            var key2LeftOverLength = key2.Length - blockCiphertext.Length;
-            
-            // Replace first 8 bytes of the challenge with last 8 bytes of key2 
-            key2.Slice(16, key2LeftOverLength).CopyTo(challengeBuffer);
-
-            // Encrypt last 8 bytes of key2
-            // ==============================
-            // This is done because the AES block size is 16 bytes and not 24 bytes
-            // If the original implementation actually used AES-CFB, we would be able to just EncryptCfb
-            // the entire key2 at once, but they are rotating the IV instead 
-            aes.EncryptEcb(
-                aesIv,
-                blockCiphertext,
-                PaddingMode.Zeros
-            );
-            blockCiphertext.Xor(challengeBuffer);
-            
-            blockCiphertext[..key2LeftOverLength].CopyTo(encryptedBlobData[offset..]);
-            offset += key2LeftOverLength;
-
-            #endregion
-
-            #region Checksum
-            
-            blockCiphertext[key2LeftOverLength..].Clear();
-            checksum.Xor(blockCiphertext);
-            Transform4.Execute(checksum, checksum, lookupTable);
-            
-            var encryptedBytes = encryptedChallengeLength + key2.Length;
-            var checksumDwords = MemoryMarshal.Cast<byte, uint>(checksum);
-            checksumDwords[3] ^= (uint)encryptedBytes;
-            
-            Transform4.Execute(checksum, checksum, lookupTable);
-
-            aes.Key = checksumEncryptionKey;
-            aes.EncryptEcb(
-                checksum,
-                blockCiphertext,
-                PaddingMode.Zeros
-            );
-
-            // Copy the final checksum
-            blockCiphertext.CopyTo(encryptedBlobData[offset..]);
-            
-            #endregion
-        }
-        finally
-        {
-            // I think it is a good practise to clear encryption keys
-            AesKeyPool.Return(challengeEncryptionKey, clearArray: true);
-            AesKeyPool.Return(checksumEncryptionKey, clearArray: true);
-        }
-        
         KeyUtilities.DeriveSessionKey(sessionKey, key1, challenge);
     }
 
